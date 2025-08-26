16fd6a823386539b76f3a4d361714d28933ec571
Ludovic Morin <ludovic@abstra208.com>
2025-08-26 00:27:24 -0400

    Fuck eslint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/app/components/header";
-import Footer from "@/app/components/footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Créalab",
